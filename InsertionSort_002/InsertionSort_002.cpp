71a933a4a143497a178be35e5243d36e37b13262
suryaguntursuprapto <suryaguntur2000@gmail.com>
2023-03-23 10:34:27 +0700

    Add project files.

---
+++ InsertionSort_002/InsertionSort_002.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+
+int arr[20];      //membuat array dengan panjang data 20
+int n;         //membuat variable inputan n
+
+void input() {    //procedure input
+	while (true) 
+	{
+		cout << "Masukkan Jumlah Data Pada Array : "; //membuat inputan jumlah element array
+		cin >> n;       //memanggil variable inputan n
+
+		if (n <= 20)      //membuat kondisi n tidak lebih dari 20
+		{  
+			break;
+		}
+		else {
+			cout << "\nArray yang anda masukkan maksimal 20 Elemen.\n"; //menampilkan pesan jika data lebih dari 20
+		}
+	}
+	cout << endl;
+	cout << "======================" << endl;
+	cout << "Masukkan Element Array" << endl;
+	cout << "======================" << endl;
+
+	for (int i = 0; i < n; i++) 
+	{
+
+	}
+}
